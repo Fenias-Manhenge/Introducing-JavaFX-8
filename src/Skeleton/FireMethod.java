5ce65beb97b66777550bd053e083be3598d6d2f4
Fenias-Manhenge <53791616+Fenias-Manhenge@users.noreply.github.com>
2021-05-28 14:48:41 +0200

    Fire Method!
    fire() ... invert the state of an Button or subbotton!

---
+++ src/Skeleton/FireMethod.java
@@ -0,0 +1,69 @@
+package Skeleton;
+
+/**
+ *
+ * @author fenia
+ */
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.FlowPane;
+
+public class FireMethod extends Application{
+    
+    private Button btnFire;
+    private CheckBox cbAc, cbPc;
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage myStage){
+        myStage.setTitle("Fire Method");
+        
+        FlowPane rootNode = new FlowPane(Orientation.VERTICAL, 0, 10);
+        
+        rootNode.setAlignment(Pos.CENTER_LEFT);
+        
+        rootNode.setPadding(new Insets(0, 0, 0, 10));
+        
+        Scene myScece = new Scene(rootNode, 300, 100);
+        
+        myStage.setScene(myScece);
+        
+        cbAc = new CheckBox("Ar Condicionado");
+        cbPc = new CheckBox("Computer");
+        
+        btnFire = new Button("Select all");
+        
+        actions();
+        
+        rootNode.getChildren().addAll(cbAc, cbPc, btnFire);
+        
+        myStage.show();
+    }
+    
+    public void actions(){
+        if(btnFire.getText().equalsIgnoreCase("Select all")){
+            btnFire.setOnAction((event) -> {
+                cbAc.fire();
+                cbPc.fire();
+                btnFire.setText("Unselect all");
+            });
+        } else {
+            if(btnFire.getText().startsWith("Se")){
+                btnFire.setOnAction((event) -> {
+                    cbAc.fire();
+                    cbPc.fire();
+                    btnFire.setText("Select all");
+                });
+            }
+        }
+    }
+}
