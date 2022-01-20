a07e9699be15a4bdd2e803879c9cf82a0b4f8135
Fenias-Manhenge <53791616+Fenias-Manhenge@users.noreply.github.com>
2022-01-20 04:47:55 +0200

    Slider!

--- src/JavaFx_controls_Part2/SliderDemo.java
+++ src/JavaFx_controls_Part2/SliderDemo.java
@@ -1,13 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package JavaFx_controls_Part2;
 
 /**
  *
  * @author fenia
  */
-public class SliderDemo {
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.control.*;
+import javafx.scene.paint.Color;
+import javafx.geometry.*;
+
+public class SliderDemo  extends Application{
     
+    Slider slider;
+    
+    Label lblResponse;
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage myStage){
+        
+        myStage.setTitle("Slider");
+        
+        FlowPane rootNode = new FlowPane(Orientation.VERTICAL, 0, 20);
+        rootNode.setPadding(new Insets(10));
+        rootNode.setAlignment(Pos.CENTER);
+        
+        Scene myScene = new Scene(rootNode, 350, 200, Color.BEIGE);
+        
+        myStage.setScene(myScene);
+        
+        slider = new Slider(0, 50, 5);
+        
+        slider.setShowTickMarks(true);
+        slider.setShowTickLabels(true);
+        slider.setSnapToTicks(true);
+        slider.setBlockIncrement(1);
+        slider.setMajorTickUnit(10);
+        slider.setMinorTickCount(9);
+        slider.setPrefSize(300, 40);
+        
+        lblResponse = new Label("Current value: " + slider.getValue());
+        
+        slider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            lblResponse.setText("Current value is: " + newValue);
+        });
+        
+        rootNode.getChildren().addAll(slider, lblResponse);
+        
+        myStage.show();
+    }
 }
