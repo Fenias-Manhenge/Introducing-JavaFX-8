29930126b05dbf0476717431fe6a30c6a91b4dd8
Fenias-Manhenge <53791616+Fenias-Manhenge@users.noreply.github.com>
2022-02-03 23:36:05 +0200

    TableView!
    Criando a classe de entrada de Dados!

---
+++ src/JavaFx_controls_Part2/ProjectEntry.java
@@ -0,0 +1,47 @@
+package JavaFx_controls_Part2;
+
+/**
+ *
+ * @author fenia
+ */
+
+import javafx.beans.property.*;
+
+public class ProjectEntry {
+    
+    // Declaring proprerties
+    private SimpleStringProperty programmer;
+    private SimpleStringProperty status;
+    private SimpleIntegerProperty size;
+
+    // Constructor
+    public ProjectEntry(String programmer, String status, int size) {
+        this.programmer = new SimpleStringProperty(programmer);
+        this.status = new SimpleStringProperty(status);
+        this.size = new SimpleIntegerProperty(size);
+    }
+
+    public String getProgrammer() {
+        return programmer.get();
+    }
+
+    public void setProgrammer(String programmer) {
+        this.programmer.set(programmer);
+    }
+
+    public String getStatus() {
+        return status.get();
+    }
+
+    public void setStatus(String status) {
+        this.status.set(status);
+    }
+
+    public int getSize() {
+        return size.get();
+    }
+
+    public void setSize(int size) {
+        this.size.set(size);
+    }
+}
---
+++ src/JavaFx_controls_Part2/TableViewDemo.java
@@ -0,0 +1,18 @@
+package JavaFx_controls_Part2;
+
+/**
+ *
+ * @author fenia
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.FlowPane;
+import javafx.geometry.*;
+import javafx.beans.property.*;
+
+public class TableViewDemo {
+    
+}
